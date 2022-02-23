3e4e79e76733ff59bf90e7f5293f6f455bbbe009
Vespatian <33759261+Vespatian@users.noreply.github.com>
2022-02-23 15:17:48 -0600

    Update plugin.py

--- Schluter-1.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Schluter-1.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -1,1 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+####################
+# Copyright (c) 2014, Perceptive Automation, LLC. All rights reserved.
+# http://www.indigodomo.com
 
+import indigo
+
+import os
+import sys
+
+class Plugin(indigo.PluginBase):
+	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
+		super(Plugin, self).__init__(pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
