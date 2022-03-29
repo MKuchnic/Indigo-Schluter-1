e7d9a43f16e0ff665e38f3f3dda4605562baa5cf
Vespatian <33759261+Vespatian@users.noreply.github.com>
2022-03-29 15:14:58 -0500

    Add files via upload

--- Schluter-1.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Schluter-1.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -18,38 +18,55 @@ from authenticator import Authenticator, Authentication
 class Plugin(indigo.PluginBase):
 	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
 		super(Plugin, self).__init__(pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
-		pfmt = logging.Formatter('%(asctime)s.%(msecs)03d\t[%(levelname)8s] %(name)20s.%(funcName)-25s%(msg)s', datefmt='%Y-%m-%d %H:%M:%S')
-		self.plugin_file_handler.setFormatter(pfmt)
 		
-		self.schluter = Schluter()
-		self.logLevel = int(self.pluginPrefs.get(u"logLevel", logging.INFO))
-		self.indigo_log_handler.setLevel(self.logLevel)
-		self.logger.debug(u"logLevel = {}".format(self.logLevel))
+#        self.logLevel = int(self.pluginPrefs["logLevel"])
+#        self.indigo_log_handler.setLevel(self.logLevel)
+#        self.logger.debug(u"logLevel = {}".format(self.logLevel))
 	
 	def startup(self):
 		indigo.server.log("Starting Schluter")
-
+		
+		self.schluter = Schluter()
+		
+		self.updateFrequency = float(self.pluginPrefs.get('updateFrequency', "15")) *  60.0
+		self.next_update = time.time() + self.updateFrequency
+		
+		self.update_needed = False
 	
 	def shutdown(self):
 		indigo.server.log("Stopping Schluter")
-
+	
+	def validatePrefsConfigUi(self, valuesDict):
+		authenticator = Authenticator(self.schluter, valuesDict["login"], valuesDict["password"])
+		authentication = authenticator.authenticate()
+		
+		errorDict = indigo.Dict()
+		errorDict["showAlertText"] = "Invalid Login or Password."
+		
+		if authentication.state.value != "authenticated":
+			return (False, valuesDict, errorDict)
+		
+		return True
 	
 	########################################
-	# Actions defined in MenuItems.xml:
-	####################
-	def testMethod(self):
-		indigo.server.log("Test Method Log")
-		output = "Test Method menu Item, login:" + self.pluginPrefs["login"] + " password:" + self.pluginPrefs["password"]
-		indigo.server.log(output)
 	
-	def printPluginPrefs(self):
-		output = "Login: " + self.pluginPrefs["login"] + " Password: " + self.pluginPrefs["password"]
-		indigo.server.log(output)
-	
-	def test_device_method(self):
-		indigo.server.log("Test Device Method Log")
+	def runConcurrentThread(self):
+		try:
+			while True:
+				if (time.time() > self.next_update) or self.update_needed:
+					self.update_needed = False
+					self.next_update = time.time() + self.updateFrequency
+					
+					indigo.server.log("Schluter - Periodic Update")
 
-	def myListGenerator(self, filter="", valuesDict=None, typeId="", targetId=0):
+				self.sleep(60.0)
+				
+		except self.StopThread:
+			pass
+	
+	########################################
+	
+	def serialNumberListGenerator(self, filter="", valuesDict=None, typeId="", targetId=0):
 		authenticator = Authenticator(self.schluter, self.pluginPrefs["login"], self.pluginPrefs["password"])
 		authentication = authenticator.authenticate()
 		
@@ -63,21 +80,19 @@ class Plugin(indigo.PluginBase):
 			serial_numbers.append(tuple)
 		
 		return serial_numbers
-
-	def validatePrefsConfigUi(self, valuesDict):
-		authenticator = Authenticator(self.schluter, valuesDict["login"], valuesDict["password"])
-		authentication = authenticator.authenticate()
-		self.logger.debug(u"validatePrefsConfigUi called")
-		errorDict = indigo.Dict()
-		
-		if authentication.state.value != "authenticated":
-			errorDict["showAlertText"] = "Invalid Login or Password."
-		
-		updateFrequency = int(valuesDict['updateFrequency'])
-		if (updateFrequency < 3) or (updateFrequency > 60):
-			errorDict['updateFrequency'] = u"Update frequency is invalid - enter a valid number (between 3 and 60)"
-		
-		if len(errorDict) > 0 :
-			return (False, valuesDict, errorDict)
-		
-		return True
+	
+	########################################
+	# Actions defined in MenuItems.xml:
+	########################################
+	
+	def testMethod(self):
+		indigo.server.log("Test Method Log")
+		output = "Test Method menu Item, login:" + self.pluginPrefs["login"] + " password:" + self.pluginPrefs["password"]
+		indigo.server.log(output)
+	
+	def printPluginPrefs(self):
+		output = "Login: " + self.pluginPrefs["login"] + " Password: " + self.pluginPrefs["password"]
+		indigo.server.log(output)
+	
+	def test_device_method(self):
+		indigo.server.log("Test Device Method Log")
