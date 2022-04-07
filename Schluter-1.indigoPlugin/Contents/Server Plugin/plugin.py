6c51789731a0813159bfb9d0bc8683940ecc66d0
Mark Kuchnicki <87202952+MKuchnic@users.noreply.github.com>
2022-04-06 22:15:20 -0500

    Update plugin.py

--- Schluter-1.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Schluter-1.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -249,7 +249,7 @@ class Plugin(indigo.PluginBase):
 		index = 1 # Not sure if this thermostat can even have more than 1 temp sensor
 		stateKey = "temperatureInput" + str(index)
 		value = self.temperatureFormatter.convertFromSchluter(thermostat.temperature)
-		displayText = self.temperatureFormatter.format(thermostat.temperature)
+		displayText = "%.1f°C" % (value)
 		self.logger.debug("displayText: %s",self.temperatureFormatter.format(thermostat.temperature))
 
 		self.logger.debug("_changeTempSensorValue: value = {}, uiValue = {}".format(value, displayText))
