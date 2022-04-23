5bb357628afbf7813afc30b751bb94195e583e92
Mark Kuchnicki <87202952+MKuchnic@users.noreply.github.com>
2022-04-23 17:38:23 -0500

    Update plugin.py

--- Schluter-1.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Schluter-1.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -354,7 +354,7 @@ class Plugin(indigo.PluginBase):
 # needto convert deviceID to serial number
 # device not defined here need a different call?
 		thermodevice = indigo.devices[deviceId]
-		serialNumber = thermodevice.address
+		serialNumber = thermodevice.pluginProps.get("serialNumbers", False)
 		self.logger.debug("serialNumber {}".format(serialNumber))
 #		self.schluter.return_to_schedule(self.authentication.session_id, serialNumber)
 		return True
