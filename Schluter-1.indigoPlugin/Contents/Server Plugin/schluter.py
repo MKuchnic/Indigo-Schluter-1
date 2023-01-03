c9da2eda56c962827077e25475e5f35d7d3a53fe
Mark Kuchnicki <87202952+MKuchnic@users.noreply.github.com>
2023-01-02 22:35:05 -0600

    Update schluter.py

--- Schluter-1.indigoPlugin/Contents/Server Plugin/schluter.py
+++ Schluter-1.indigoPlugin/Contents/Server Plugin/schluter.py
@@ -126,6 +126,7 @@ class Schluter:
                 if response.status_code == 401 :
                     self.auth_update_needed =  True
                     self.logger.error("Authetication Error - will re-authenticate")
+                    self.logger.error("self.auth_update_needed: {}".format(self.auth_update_needed))
                 return None
 
             self.logger.debug("API Response received: %s - %s", response.status_code, response.content)
